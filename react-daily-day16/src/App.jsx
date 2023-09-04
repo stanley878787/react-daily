f5e668726306746e925345c77a0d3e8b1370e97b
stanley878787 <a0968710686@gmail.com>
2023-09-04 17:54:10 +0800

    finish

--- react-daily-day16/src/App.jsx
+++ react-daily-day16/src/App.jsx
@@ -1,7 +1,52 @@
-function App(){
-    return <>
-    <h1>87</h1>
-    </>
+import './App.css';
+import { HashRouter, NavLink , Routes, Route, useNavigate} from 'react-router-dom';
+
+const Todo = () => {
+  return <p>這是 Todo 頁面</p>;
+};
+const Login = () => {
+  return <p>這是登入頁面</p>;
+};
+const Register = () => {
+  return <p>這是註冊頁面</p>;
+};
+
+function App() {
+  return (
+    <div className="container">
+      <HashRouter>
+        <div className="nav-link">
+          <NavLink to="/">
+            <p>回到首頁</p>
+          </NavLink>
+          <NavLink to="/register">
+            <p>註冊頁面</p>
+          </NavLink>
+          <NavLink to="/login">
+            <p>登入頁面</p>
+          </NavLink>
+          <NavLink to="/todo">
+            <p>Todo 頁面</p>
+          </NavLink>
+        </div>
+        {/* Routes, Route 練習區 */}
+        <Routes>
+            <Route path="/" element='' />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/todo" element={<Todo />} />
+            <Route
+              path="*"
+              element={
+                <main style={{ padding: "1rem" }}>
+                  <p>No Thing 87</p>
+                </main>
+              }
+            />
+        </Routes>
+      </HashRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- react-daily-day16/vite.config.js
+++ react-daily-day16/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/儲存庫名稱/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/react-daily/' : '/',
   plugins: [react()],
 })
